887d17474a3d8c587bb0aa745ca65f0e08c1a8b7
stashaway <sloan.tash@yahoo.com>
2017-01-13 10:58:15 -0800

    Angular Routes - Sloan Tash

--- ang_routes/script.js
+++ ang_routes/script.js
@@ -1,22 +1,39 @@
 // Create the route module and name it routeApp
-
+var app = angular.module('routeApp',["ngRoute"]);
 // Config the routes
-
-         // route for the home page
-
-        // route for the about page
-
-        // route for the contact page
-
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl: 'pages/home.html',
+            controller: 'homeCtrl'
+        }).when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'aboutCtrl'
+        }).when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'contactCtrl'
+    }).otherwise({
+        redirectTo: '/'
+    });
+    // route for the home page
+    // route for the about page
+    // route for the contact page
+});
 
 // Create the controllers for the different pages below
+app.controller('routeCtrl', function($scope) {
+    $scope.message = "lorem ipsum";
+}).controller('homeCtrl', function($scope) {
+    $scope.message = "Main page reached";
+}).controller('aboutCtrl', function($scope) {
+    $scope.message = "About our stuffs!";
+}).controller('contactCtrl',function($scope) {
+    $scope.message = "Talk to me";
+});
 
 // home page controller
     // Create a message to display in the view
-
-
 // about page controller
     // Create a message to display in the view
-
 // contact page controller
     // Create a message to display in the view
