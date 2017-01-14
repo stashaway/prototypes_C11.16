3ac4bcd983ceb1305e80b95a920fbe0391bfe0c0
stashaway <sloan.tash@yahoo.com>
2017-01-13 16:49:18 -0800

    changed to async

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -5,12 +5,17 @@ function handleRequest(request,response){
     console.log('Node console notification - handling request');
     // response.write('Booyah! You get a fritter, all-star!\n');
     // response.write('Path hit: ' + request.url);
-    var file=fs.readFileSync('public/index.html');
-    response.end(file);
+    fs.readFile('public/index.html',function(err, data){
+        if (err) {
+            console.log('Error!'+err);
+        } else {
+            response.end(data);
+        }
+    });
 }
-function handleError(err,data){
-    console.log('Error!',data);
-}
+// function handleError(err,data){
+//     console.log('Error!',data);
+// }
 var server = http.createServer(handleRequest);
 server.listen(8086, function(){
     console.log("Server listening on: http://localhost:%s", 8086);
