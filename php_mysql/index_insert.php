fa6d63b5770a22b59abdc3e6c56f3846a737885c
stashaway <sloan.tash@yahoo.com>
2017-01-18 09:59:01 -0800

    PHP MySQL - Sloan Tash

--- php_mysql/index_insert.php
+++ php_mysql/index_insert.php
@@ -2,7 +2,7 @@
 <?php
 require_once('mysql_connect.php');
 print_r($_POST);
-$query = "INSERT INTO `todo_items` SET `title`=$_POST[title], `details`=$_POST[details],`timestamp`=$_POST[timestamp],`progress`=$_POST[progress]";
+$query = "INSERT INTO `todo_items` SET `title`=\"{$_POST['title']}\", `details`=\"{$_POST['details']}\",`timestamp`={$_POST['timestamp']},`progress`=\"{$_POST['progress']}\"";
 $result = mysqli_query($conn, $query);
 $numb = mysqli_affected_rows($conn);
 print(''.$numb.' rows affected');
